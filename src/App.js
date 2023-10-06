b8a1c4ad56c40f9de97b6c3bc2545c7df7efef23
Kreshna Putra Adi Wicaksana <kreshnaputraadi31@student.uns.ac.id>
2023-10-06 17:06:02 +0700

    Update

--- src/App.js
+++ src/App.js
@@ -1,6 +1,9 @@
-import React from 'react';
-import Home from './components/Home'; // Sesuaikan path dengan lokasi file Beranda.js Anda
-
+import React from "react";
+import Home from "./components/page/Home"; // Sesuaikan path dengan lokasi file Beranda.js Anda
+// import './components/Home.css'
+import "./App.css";
+import "./components/css/Home.css";
+import "bootstrap/dist/css/bootstrap.min.css";
 
 function App() {
   return (
