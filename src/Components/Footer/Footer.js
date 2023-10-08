b8f8b6606f17e8aa971ba5c7c4f38a1401fce1ac
Kreshna Putra Adi Wicaksana <kreshnaputraadi31@student.uns.ac.id>
2023-10-08 16:10:46 +0700

    Update New

--- src/Components/Footer/Footer.js
+++ src/Components/Footer/Footer.js
@@ -3,64 +3,64 @@ import "./Footer.css";
 
 const Footer = () => {
   return (
-    <div class="main-wrap">
-      <div class="footer-wrap">
-        <div class="footer-wrap-1">
-          <div class="footer-section">
+    <div className="main-wrap">
+      <div className="footer-wrap">
+        <div className="footer-wrap-1">
+          <div className="footer-section">
             <h1>About</h1>
             <a href="#">Services</a>
             <a href="#">Portfolio</a>
             <a href="#">Pricing</a>
             <a href="#">Careers</a>
           </div>
-          <div class="footer-section">
+          <div className="footer-section">
             <h1>Resources</h1>
             <a href="#">Docs</a>
             <a href="#">Blog</a>
             <a href="#">eBooks</a>
             <a href="#">Videos</a>
           </div>
-          <div class="footer-section">
+          <div className="footer-section">
             <h1>Contact</h1>
             <a href="#">Help</a>
             <a href="#">Sales</a>
             <a href="#">Advertise</a>
           </div>
-          <div class="footer-section">
+          <div className="footer-section">
             <h1>Stay Connected</h1>
             <p>Subscribe to our newsletter to receive latest news.</p>
             <form action="">
               <input refs="email" type="email" name="email" id="email" size="30" placeholder="Enter Email Address" />
-              <button type="submit" class="btn">
+              <button type="submit" className="btn btn-primary">
                 Subscribe
               </button>
             </form>
           </div>
         </div>
-        <div class="footer-wrap-2">
-          <div class="line"></div>
-          <div class="social-link">
+        <div className="footer-wrap-2">
+          <div className="line"></div>
+          <div className="social-link">
             <a href="#">
-              <i class="fab fa-instagram"></i>
+              <i className="fab fa-instagram"></i>
             </a>
             <a href="#">
-              <i class="fab fa-github"></i>
+              <i className="fab fa-github"></i>
             </a>
             <a href="#">
-              <i class="fab fa-facebook"></i>
+              <i className="fab fa-facebook"></i>
             </a>
             <a href="#">
-              <i class="fab fa-twitter-square"></i>
+              <i className="fab fa-twitter-square"></i>
             </a>
           </div>
         </div>
       </div>
-      <div class="footer-bottom">
-        <div class="first-box">
+      <div className="footer-bottom">
+        <div className="first-box">
           <a href="#">Terms & Conditions</a>
           <a href="#">Privacy Policy</a>
         </div>
-        <div class="last-box">
+        <div className="last-box">
           <a href="#">&copy; Copyright 2023 Job Connect</a>
         </div>
       </div>
